096f2b55d8516c4d5822bbff94123dd995f4f0fe
Lucautz <127567616+LucasPMartins@users.noreply.github.com>
2023-05-05 10:42:27 -0300

    Update atividade_lab.c

--- EXTRA/atividade_lab.c
+++ EXTRA/atividade_lab.c
@@ -1,42 +1,32 @@
-#include <stdio.h>
-#include <string.h>
+#include<STDIO.H>
+#include<string.h>
 
-int main()
-{
-    char string[100];
-    setbuf(stdin, NULL);
-
+int main (){
+    char str[100];
     int i;
-    // Faça um programa em C que, dada uma string de entrada,
-    //  gere uma string de saída com todos ‘r’ e ‘rr’ trocados por ‘l’, exceto para os
-    //  casos que o ‘r’ está no final das palavras.
-    fgets(string, 100, stdin);
-    int tamanho = strlen(string);
-    
-    for (i = 0; i < tamanho; i++)
-    {
-        if (string[i] == 'r' && (string[i + 1] < 'a' || string[i + 1] > 'z') && (string[i + 1] < 'A' || string[i + 1] > 'Z'))
-        {
+    printf("Digite uma frase:");
+    fgets(str,100,stdin);
+    int tamanho = strlen(str);
+    for(i = 0;i<tamanho;i++){
+        if(str[i]=='r'&&(str[i+1]<'a'||str[i+1]>'z')&&(str[i+1]<'A'||str[i+1]>'Z')){
             continue;
         }
-        else if (string[i] == 'r' && string[i + 1] == 'r')
+        if (str[i] == 'r' && str[i+1] == 'r')
         {
-            for (int j = i; j < tamanho; j++)
-            {
-                if (j == i)
-                {
-                    string[i] = string[i + 1];
-                }
-                string[j] = string[j + 1];
+            for(int j = i+1;j<tamanho;j++){
+            if(j==i){
+                str[i]=str[i+1];
             }
-            string[i] = 'l';
+            str[j]=str[j+1];
+            }
         }
-
-        else if (string[i] == 'r')
-        {
-            string[i] = 'l';
+        if(str[i]=='r'){
+            str[i]='l';
+        }
+        else if(str[i]=='R'){
+            str[i]='L';
         }
     }
-    printf("%s", string);
+    printf("%s",str);
     return 0;
 }
